7ac23d21108a24270b936b415a27acd5fcf198d1
Arsenii <arsenii.nedorezov@gmail.com>
2026-05-08 18:33:08 +0200

    Make bigger sprites and display a variable hitbox

--- src/ch/hevs/gdx2d/research/ResearchHello.scala
+++ src/ch/hevs/gdx2d/research/ResearchHello.scala
@@ -18,54 +18,62 @@ class ResearchHello(width: Int, height: Int) extends PortableApplication(width, 
   val rectWidth: Int = 87
   val rectHeight: Int = 136
   var isMoving: Boolean = false
-  var spritesheet: Spritesheet = null;
+  var spritesheetIdle: Spritesheet = _
+  var spritesheetWalk: Spritesheet = _
   var currentFrame = 0
   var dt = 0.0
-  var nFrames = 9
-  var FRAME_TIME = 0.1
+  var nFramesWalk = 9
+  var nFramesIdle = 7
+  var FRAME_TIME = 0.07
   var direction: Int = 1
   override def onInit(): Unit = {
     setTitle("Research Time")
-    rectPosition = new Vector2(200, (getWindowHeight.toDouble * 0.2).toInt)
-    spritesheet = new Spritesheet("data/images/Walk.png", 87, 136)
+    spritesheetIdle = new Spritesheet("data/images/Idle.png", 152, 268)
+    spritesheetWalk = new Spritesheet("data/images/Walk.png", 174, 272)
+    println("INIT DONE")
   }
 
   override def onKeyDown(keycode: Int): Unit = {
     if (keycode == 32 || keycode == 22 || keycode == 29 || keycode == 21) {
       isMoving = true
+      dt = 0
+      currentFrame = 0
       if (keycode == 29 || keycode == 21) direction = -1 else direction = 1
     }
   }
 
   override def onKeyUp(keycode: Int): Unit = {
     if (keycode == 32 || keycode == 22 || keycode == 29 || keycode == 21) {
+      dt = 0
+      currentFrame = 0
       isMoving = false
     }
   }
 
   private def Update(): Unit = {
     if (isMoving) {
-      rectPosition.add(new Vector2(direction * 10, 0))
+      rectPosition.add(new Vector2(direction * 20, 0))
     }
   }
 
   override def onGraphicRender(g: GdxGraphics): Unit = {
     g.clear()
 
+    println("RENDERING GRAPHICS")
     dt += Gdx.graphics.getDeltaTime
 
     // Do we have to display the next frame// Do we have to display the next frame
     if (dt > FRAME_TIME) {
       dt = 0
-      currentFrame = (currentFrame + 1) % nFrames
+      currentFrame = (currentFrame + 1) % (if (isMoving) nFramesWalk else nFramesIdle)
 
       Update()
     }
 
-    g.drawFilledRectangle(rectPosition.x + rectWidth / 2, rectPosition.y + rectHeight / 2, rectWidth, rectHeight, 0, Color.LIME)
+    val activeSprite = if (isMoving) spritesheetWalk else spritesheetIdle
+    g.drawFilledRectangle(rectPosition.x + activeSprite.sprites(0)(currentFrame).getRegionWidth / 2, rectPosition.y + activeSprite.sprites(0)(currentFrame).getRegionHeight / 2, activeSprite.sprites(0)(currentFrame).getRegionWidth, activeSprite.sprites(0)(currentFrame).getRegionHeight, 0, Color.LIME)
 
-    g.draw(spritesheet.sprites(0)(currentFrame), rectPosition.x, rectPosition.y) // Centered
-
+    g.draw(activeSprite.sprites(0)(currentFrame), rectPosition.x, rectPosition.y)
     g.drawFPS()
   }
 }
